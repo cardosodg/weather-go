6be7971903f5e935908b44b4fd9031a209123a89
Diego <>
2026-01-08 09:42:55 -0300

    Add timezone in url call

--- internal/collector/config/config.go
+++ internal/collector/config/config.go
@@ -2,8 +2,8 @@ package config
 
 const (
 	OpenMeteoParams  = "temperature_2m,relative_humidity_2m,rain,apparent_temperature"
-	OpenMeteoBaseURL = "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current=%s"
-	OpenMeteoHistURL = "https://api.open-meteo.com/v1/forecast?latitude=%s&past_days=60&longitude=%s&minutely_15=%s"
+	OpenMeteoBaseURL = "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current=%s&timezone=UTC"
+	OpenMeteoHistURL = "https://api.open-meteo.com/v1/forecast?latitude=%s&past_days=60&longitude=%s&minutely_15=%s&timezone=UTC"
 
 	LocationsFile = "./locations.json"
 
